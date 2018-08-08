ea601e01c1e11bb49aa42215a3b5070f119348ea
전진호 <gino9940@gmail.com>
2018-08-08 20:00:46 +0900

    Readme + App.json

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { TextInput } from "./node_modules/react-native-gesture-handler";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<StatusBar barStyle="light-content"></StatusBar>
+				<Title style={styles.title}>Kawai Todo</Title>
+				<View style={styles.card}>
+					<TextInput style={styles.input} placeholder={"New To Do"} />
+				</View>?
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#F23657",
+		alignItems: "center"
+	},
+	title: {
+		color: "white",
+		fontSize: 30,
+		marginTop: 50,
+		fontWeight: 200
+	}
 });
